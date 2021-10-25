eefc51c20c0e7accc7a41bb0ae744f6e2338dd13
Nate <minecraftcrusher100@gmail.com>
2021-10-25 00:32:22 +0000

    Added setup.py

--- regexfactory/__init__.py
+++ regexfactory/__init__.py
@@ -17,3 +17,10 @@ from .patterns import (
     Amount,
     Optional
 )
+
+
+__name__ = "RegexFactory"
+__version__ = "0.0.0"
+__description__ = "Dynamically generate regex patterns"
+__author__ = "GrandMoff100"
+__author_email__ = "nlarsen23.student@gmail.com"
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+from regexfactory import (
+    __name__,
+    __description__,
+    __version__,
+    __author__,
+    __author_email__
+)
+
+setup(
+    name=__name__,
+    version=__version__,
+    description=__description__,
+    author=__author__,
+    author_email=__author_email__,
+    packages=["regexfactory"],
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown"
+)

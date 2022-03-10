3a0c51ef8d8f1ca7f667f707afff56d2040d535f
GrandMoff100 <minecraftcrusher100@gmail.com>
2022-03-10 22:06:21 +0000

    Made Amount init type overloads more concise

--- regexfactory/patterns.py
+++ regexfactory/patterns.py
@@ -52,18 +52,8 @@ class NotSet(RegexPattern):
 
 
 class Amount(RegexPattern):
-    @overload
-    def __init__(
-        self, pattern: ValidPatternType, repetitions: int, or_more: bool = False
-    ) -> None:
-        ...
-
-    @overload
-    def __init__(self, pattern: ValidPatternType, minimum: int, maximum: int) -> None:
-        ...
-
-    def __init__(self, pattern: ValidPatternType, *args, **kwargs) -> None:
-        i, j, or_more = self.parse_init_args(*args, **kwargs)
+    def __init__(self, pattern: ValidPatternType, i: int, *args: int, or_more: bool = False) -> None:
+        j, *_ = args + (None,)
 
         if j is not None:
             amount = f"{i},{j}"
@@ -74,45 +64,6 @@ class Amount(RegexPattern):
 
         regex = self.get_regex(pattern) + "{" + amount + "}"
         super().__init__(regex)
-
-    @staticmethod
-    def parse_init_args(*args, **kwargs) -> t.Tuple[int, t.Optional[int], bool]:
-        s1 = inspect.Signature(
-            parameters=(
-                inspect.Parameter(
-                    "repetitions", kind=inspect.Parameter.POSITIONAL_OR_KEYWORD
-                ),
-                inspect.Parameter(
-                    "or_more",
-                    kind=inspect.Parameter.POSITIONAL_OR_KEYWORD,
-                    default=False,
-                ),
-            )
-        )
-        s2 = inspect.Signature(
-            parameters=(
-                inspect.Parameter(
-                    "minimum", kind=inspect.Parameter.POSITIONAL_OR_KEYWORD
-                ),
-                inspect.Parameter(
-                    "maximum", kind=inspect.Parameter.POSITIONAL_OR_KEYWORD
-                ),
-            )
-        )
-        try:
-            bound = s1.bind(*args, **kwargs)
-            bound.apply_defaults()
-            i = bound.arguments["repetitions"]
-            j = None
-            or_more = bound.arguments["or_more"]
-        except TypeError:
-            bound = s2.bind(*args, **kwargs)
-            bound.apply_defaults()
-            i = bound.arguments["minimum"]
-            j = bound.arguments["maximum"]
-            or_more = False
-
-        return i, j, or_more
 
 
 class Optional(RegexPattern):

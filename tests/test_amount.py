c030d59852f09a312b66024a45b209f8fae2da56
Francis R. Kovacs <frankkovacs8@gmail.com>
2023-06-25 21:01:03 -0400

    make bounds test more specific

--- tests/test_amount.py
+++ tests/test_amount.py
@@ -1,14 +1,15 @@
 from regexfactory import Amount
 import pytest
-from hypothesis import given, strategies as st
-from tests.strategies import build_amount_greedy, build_bounds
+from hypothesis import given, settings, strategies as st
+from tests.strategies import build_amount_greedy, build_bounds, build_amount
 
 
 @pytest.mark.patterns
 @given(st.text(min_size=1), st.integers(min_value=1))
-def test_amount(word, count):
+def test_amount_single_count(word, count):
     actual = Amount(word, i=count)
     assert actual.regex == "{word}{{{count}}}".format(word=word, count=str(count))
+
 
 
 @pytest.mark.patterns
@@ -22,7 +23,6 @@ def test_amount(word, count):
 )
 def test_amount_lower_upper(word, bound: range):
     actual = Amount(word, bound.start, bound.stop)
-    str(actual)
     expected = "{word}{{{lower},{upper}}}".format(word=word, lower=str(bound.start), upper=str(bound.stop))
     assert actual.regex == expected
 
@@ -35,10 +35,19 @@ def test_amount_or_more(word, count):
 
 
 @pytest.mark.patterns
-@given(st.text(min_size=1), st.integers(min_value=1))
-def test_amount_non_greedy(word, count):
-    actual = Amount(word, count, greedy=False)
-    assert actual.regex.endswith("?")
+@given(st.builds(
+    lambda pattern, start, or_more, step: build_amount(pattern=pattern, start=start, or_more=or_more, greedy=False, step=step),
+    st.text(min_size=1),
+    st.integers(min_value=1, max_value=5),
+    st.integers(min_value=1),
+    st.booleans()
+))
+@settings(max_examples=500)
+def test_amount_non_greedy(amt):
+    """
+    Test to ensure that instances of Amount with greedy as False will end with "?"
+    """
+    assert amt.regex.endswith("?")
 
 
 @given(st.builds(

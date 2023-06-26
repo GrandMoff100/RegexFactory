ee9759e4685ef75ac9bd3a1c20fb90b72b59dc50
Francis R. Kovacs <frankkovacs8@gmail.com>
2023-06-25 21:00:31 -0400

    update build_amount to use optional stpe

--- tests/strategies.py
+++ tests/strategies.py
@@ -1,6 +1,8 @@
 from regexfactory import Amount
-from regexfactory.pattern import ESCAPED_CHARACTERS
+from regexfactory.pattern import ESCAPED_CHARACTERS, ValidPatternType
 from string import printable
+from typing import Optional
+
 
 ## Variable that defines all characters that are not used in escapes.
 non_escape_printable = "".join(
@@ -14,6 +16,26 @@ def build_bounds(lower_bound, step) -> range:
     """
     upper_bound = lower_bound + step
     return range(lower_bound, upper_bound + 1)
+
+
+def build_amount(pattern: ValidPatternType, start: int, or_more: bool, greedy: bool, step: Optional[int]):
+    """
+    General Amount builder. This function will be repurposed with hard-coded
+    values for a parameter when used with more specific use cases.
+    Note that the `j` parameter is constructed as the step plus start
+    """
+    stop_value = None
+    if step is not None:
+        stop_value = start + step
+    return Amount(
+        pattern=pattern,
+        i=start,
+        j=stop_value,
+        or_more=or_more,
+        greedy=greedy
+    )
+
+
 
 
 def build_amount_greedy(words, count, greedy):
